b22a285456e1233717d7a00d9b34ab11a90b9511
Adam McKenney <34550186+DaftBeowulf@users.noreply.github.com>
2019-07-19 12:23:14 -0500

    :bug: MVP finished, all tests OK
    Forgot to account for if items were equal, now all tests pass

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -117,19 +117,19 @@ class SortingRobot:
         self.swap_item()
         while not self.light_is_on():
             # spot is empty and can move right, so move right and pick up item there
-            if self.compare_item() == None and self.can_move_right():
-                self.move_right()
-            # if compared item is smaller, swap and move to next
-            elif self.compare_item() == 1 and self.can_move_right():
-                self.swap_item()
-                self.move_right()
-            # if held item is smaller, keep and move to next
-            elif self.compare_item() == -1 and self.can_move_right():
-                self.move_right()
-            # if at end, check last item and see if needing to swap
-            # , then move left to empty for new run
-            elif not self.can_move_right():
-                # end of list reached
+            if self.can_move_right():
+                if self.compare_item() == None:
+                    self.move_right()
+                # if compared item is smaller, swap and move to next
+                elif self.compare_item() == 1:
+                    self.swap_item()
+                    self.move_right()
+                # if held item is smaller or equal, keep and move to next
+                elif self.compare_item() == -1 or self.compare_item() == 0:
+                    self.move_right()
+            else:
+                # if at end, check last item and see if needing to swap
+                # , then move left to empty for new run
 
                 # if compared item is smaller, swap and go back for new run
                 if self.compare_item() == 1:
@@ -142,8 +142,8 @@ class SortingRobot:
 if __name__ == "__main__":
     # Test our your implementation from the command line
     # with `python robot_sort.py`
-    l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1,
-         45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
+
+    l = [1, 1, 0, 2]
 
     robot = SortingRobot(l)
 

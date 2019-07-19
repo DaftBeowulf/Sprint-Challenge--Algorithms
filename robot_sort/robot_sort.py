b786bc4c781067db7b53ec53567c1c5abe164f01
Adam McKenney <34550186+DaftBeowulf@users.noreply.github.com>
2019-07-19 11:59:30 -0500

    :sparkles: First pass works, but hangs
    large varied list hangs

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -95,11 +95,19 @@ class SortingRobot:
         return self._light == "ON"
 
     def move_left_to_empty(self):
-        # move left until finding empty spot
+        # move left until finding empty spot, drop item
         while self.compare_item() != None:
             self.move_left()
-        # then drop held item (lowest) there
         self.swap_item()
+        # if robot can still move right, we weren't in the last spot
+        if self.can_move_right():
+            # so move right, and pick up new one
+            self.move_right()
+            self.swap_item()
+        else:
+            # empty spot was on the last spot (didn't move in any move lefts)
+            # array is fully sorted, so turn light on
+            self.set_light_on()
 
     def sort(self):
         """
@@ -121,25 +129,21 @@ class SortingRobot:
             # if at end, check last item and see if needing to swap
             # , then move left to empty for new run
             elif not self.can_move_right():
-                # if compare returns none at this spot, can only be because hand is empty
-                #  and all items to the left have been sorted. finished, turn on light
-                if self.compare_item() == None:
-                    self.set_light_on()
+                # end of list reached
 
-                # compared item is smaller, swap and go back for new run
-                elif self.compare_item() == 1:
+                # if compared item is smaller, swap and go back for new run
+                if self.compare_item() == 1:
                     self.swap_item()
 
-                # compared item is bigger, so go back and drop the lowest item in the empty spot
+                # if compared item is bigger or the spot is empty, just go back for new run
                 self.move_left_to_empty()
 
 
 if __name__ == "__main__":
     # Test our your implementation from the command line
     # with `python robot_sort.py`
-    l = [1, 4, 5, 2]
-    # l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1,
-    #  45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
+    l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1,
+         45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
 
     robot = SortingRobot(l)
 
